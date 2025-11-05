abdf403dbf2c82158164cad54da0a647ff3e405a
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-11-05 21:55:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+# GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 COLLECTION_NAME = "news_articles"
 CHROMA_DIR = "./chroma_store"
